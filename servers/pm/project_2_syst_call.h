9a43e53a459e1e1743fae9ef67898a2e5977ba4a
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-03-28 12:01:42 -0500

    asdas

--- servers/pm/project_2_syst_call.h
+++ servers/pm/project_2_syst_call.h
@@ -2,4 +2,16 @@
 #ifndef PROJECT_2_SYST_CALL_H
 #define PROJECT_2_SYST_CALL_H
 
+int do_topic_lookup(void);
+
+int do_topic_create(void);
+
+int do_topic_publisher(void);
+
+int do_topic_subscriber(void);
+
+int do_topic_publish(void);
+
+int do_retrieve(void);
+
 #endif /** PROJECT_2_SYST_CALL_H */

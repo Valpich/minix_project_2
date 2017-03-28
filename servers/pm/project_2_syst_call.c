8bef0c6e63ff6b3bce982fa179ae7524e0aeb113
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-03-28 13:49:34 -0500

    Debug semaphores

--- servers/pm/project_2_syst_call.c
+++ servers/pm/project_2_syst_call.c
@@ -28,42 +28,50 @@
 #define MAX_MSG 5	/* Number of message for each topic */
 #define MAX_TOPIC 10	/* Number of allowed topics for each topic */
 typedef int semaphore;	/* Define semaphore as a type */
-static semaphore mutex[MAX_TOPIC]  = {1};	/* Controls access to critical region */
-static semaphore empty[MAX_TOPIC] = {MAX_MSG};	/* Count empty buffer slots */
-static semaphore full[MAX_TOPIC]  = {0};		/* Count full buffer slots */
+static semaphore mutex[MAX_TOPIC]  = {[0 ... MAX_TOPIC-1] = 1};	/* Controls access to critical region */
+static semaphore empty[MAX_TOPIC] = {[0 ... MAX_TOPIC-1] = MAX_MSG};	/* Count empty buffer slots */
+static semaphore full[MAX_TOPIC]  = {[0 ... MAX_TOPIC-1] = 0};		/* Count full buffer slots */
 
 
 void down(semaphore * s){
-    while(s == 0)wait();
-    s = s - 1;
+    printf("s in down is %d\n", *s);
+    while(*s == 0)wait();
+    *s = *s - 1;
 }
 
 void up(semaphore * s){
-    s = s + 1;
+    printf("s in up is %d\n", *s);
+    *s = *s + 1;
 }
 
 void enter_critical_region(int topic_id){
+    printf("Entering critical region");
     down(&mutex[topic_id]);
 }
 
 void leave_critical_region(int topic_id){
+    printf("Leaving critical region");
     up(&mutex[topic_id]);
 }
 
 void publish_lock(int topic_id){
+    printf("Entering publish lock with empty %d", empty[topic_id]);
     down(&empty[topic_id]);
     enter_critical_region(topic_id);
-    // TODO: Publish the topic
+    // TODO: Add the published topic
     leave_critical_region(topic_id);
     up(&full[topic_id]);
+    printf("Leaving publish lock with full %d", full[topic_id]);
 }
 
 void unpublish_lock(int topic_id){
+    printf("Entering publish lock with full %d", full[topic_id]);
     down(&full[topic_id]);
     enter_critical_region(topic_id);
     // TODO: Remove the published topic
     leave_critical_region(topic_id);
     up(&empty[topic_id]);
+    printf("Leaving publish lock with empty %d", empty[topic_id]);
 }
 
 int do_topic_lookup(void){

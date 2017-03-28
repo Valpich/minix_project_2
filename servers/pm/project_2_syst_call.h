46d5a6e88c45ea1cc08050f0ce3b1c1cee158b17
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-03-28 13:54:30 -0500

    Debug

--- servers/pm/project_2_syst_call.h
+++ servers/pm/project_2_syst_call.h
@@ -2,13 +2,6 @@
 #ifndef PROJECT_2_SYST_CALL_H
 #define PROJECT_2_SYST_CALL_H
 
-#define MAX_MSG 5	/* Number of message for each topic */
-#define MAX_TOPIC 10	/* Number of allowed topics for each topic */
-typedef int semaphore;	/* Define semaphore as a type */
-semaphore mutex[MAX_TOPIC]  = {1};	/* Controls access to critical region */
-semaphore empty[MAX_TOPIC] = {MAX_MSG};	/* Count empty buffer slots */
-semaphore full[MAX_TOPIC]  = {0};		/* Count full buffer slots */
-
 int do_topic_lookup(void);
 
 int do_topic_create(void);

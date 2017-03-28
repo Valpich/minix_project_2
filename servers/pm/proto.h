688186518c8118a6a9408aa29daab03f4d3c21aa
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-03-28 12:34:19 -0500

    xcvxc

--- servers/pm/proto.h
+++ servers/pm/proto.h
@@ -110,5 +110,5 @@ int do_topic_lookup(void);
 int do_topic_create(void);
 int do_topic_publisher(void);
 int do_topic_subscriber(void);
-int do_topic_publish(void;
+int do_topic_publish(void);
 int do_retrieve(void);

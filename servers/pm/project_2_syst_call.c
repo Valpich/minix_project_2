dbd03c7cfebbe7d9dbe1c674b206205083a263d9
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-03-28 13:55:24 -0500

    debug

--- servers/pm/project_2_syst_call.c
+++ servers/pm/project_2_syst_call.c
@@ -24,6 +24,7 @@
 #include <string.h>
 #include <lib.h>
 #include <malloc.h>
+#include <sys/wait.h>
 
 #define MAX_MSG 5	/* Number of message for each topic */
 #define MAX_TOPIC 10	/* Number of allowed topics for each topic */

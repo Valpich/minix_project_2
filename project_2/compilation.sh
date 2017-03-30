5ed5f8518e2f92a6470bb011f8fc83eb5e6bcf26
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-03-30 18:40:59 -0500

    cxxc

--- project_2/compilation.sh
+++ project_2/compilation.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp /usr/src/servers/pm/project_2_syst_call.c /usr/src/project_2/project_2_syst_call.c
-cc main.c project_2_syst_call -o main
+cc main.c project_2_syst_call.c -o main
 ./main

27fba170b7d3a6a13e6060a69c3c6944df7c63b7
Valentin Pichavant <valentin.pichavant@hotmail.fr>
2017-03-30 14:58:28 -0500

    Move to header

--- servers/pm/project_2_syst_call.c
+++ servers/pm/project_2_syst_call.c
@@ -1,57 +1,9 @@
 
 #include "project_2_syst_call.h"
 
-
-#include <signal.h>
-
-#include <sys/resource.h>
-#include <sys/utsname.h>
-
-#include <string.h>
-#include <stdlib.h>
-
-#include <assert.h>
-
-#include <unistd.h>
-#include <sys/types.h>
-#include <stdio.h>
-#include <string.h>
-
-#include <sys/wait.h>
-
-#define MAX_MSG 5	/* Number of message for each topic */
-#define MAX_TOPIC 10	/* Number of allowed topics for each topic */
-#define MAX_USR 30	/* Number of allowed user */
-typedef int semaphore;	/* Define semaphore as a type */
 static semaphore mutex[MAX_TOPIC]  = {[0 ... MAX_TOPIC-1] = 1};	/* Controls access to critical region */
 static semaphore empty[MAX_TOPIC] = {[0 ... MAX_TOPIC-1] = MAX_MSG};	/* Count empty buffer slots */
 static semaphore full[MAX_TOPIC]  = {[0 ... MAX_TOPIC-1] = 0};		/* Count full buffer slots */
-
-typedef struct UserTopic{
-    int id; /* Topic id */
-    char * name;    /* Topic name */
-    char * messageContent[MAX_MSG]; /* Message content of a Topic */
-    bool read[MAX_MSG]; /* If the message was read */
-    void (*toString)(const struct UserTopic *); /* Pointer to the display function of a UserTopic*/
-}UserTopic;
-
-typedef struct Subscriber{
-    pid_t pid_subscriber;   /* Subscriber pid_t */
-    struct UserTopic topic[MAX_TOPIC];   /* Topics subscribed */
-    void (*toString)(const struct Subscriber *);    /* Pointer to the display function of a Subscriber */
-}Subscriber;
-
-typedef struct Publisher{
-    pid_t pid_publisher;   /* Publisher pid_t */
-    char * topicNames[MAX_TOPIC];   /* Topics declared as published */
-    void (*toString)(const struct Publisher *);    /* Pointer to the display function of a Publisher */
-}Publisher;
-
-typedef struct Topics{
-    char * topicNames[MAX_TOPIC];    /* List of all topics */
-    bool canBeRemoved[MAX_TOPIC];    /* If a topic can be removed */
-    void (*toString)(const struct Topics *);  /* Pointer to the display function of a Topics */
-}Topics;
 
 static int messageOfTopicToRead[MAX_TOPIC][MAX_MSG] = {[0 ... MAX_TOPIC-1] = 0, [0 ... MAX_MSG-1] = 0};   /* Count the topics that  subscribed */
 static Subscriber subscribers[MAX_USR];
@@ -157,9 +109,9 @@ int do_topic_lookup(void){
 
 int do_topic_create(void){
     printf("coucou\n");
-
+    
     // create_topic(name);
-
+    
     char *name = NULL;
     // strcpy(name,m_in.m3_ca1);
     printf("received value : %s \n",name);
@@ -192,27 +144,27 @@ bool create_topic(const char * name){
         int i = 0;
         for(i=0; i< MAX_TOPIC; i++) {
             if(strcmp(name, topics.topicNames[i]) == 0) {
-                printf("Topic %s is already in list\n", name);
+                printf("Topic %s is already in list.\n", name);
                 return false;
             }else if(strcmp("\0",topics.topicNames[i]) == 0){
                 printf("Empty find at %d\n", i);
                 enter_critical_region_topic(i);
-                printf("Setting name\n");
+                printf("Setting name.\n");
                 char *a = malloc(sizeof(name));
                 strcpy(a,name);
                 topics.topicNames[i] = a;
-                printf("Topic name is %s\n",topics.topicNames[i]);
+                printf("Topic name is %s.\n",topics.topicNames[i]);
                 topicsSize++;
-                printf("Topic size is %d\n",topicsSize);
-                printf("Topic created \n");
+                printf("Topic size is %d.\n",topicsSize);
+                printf("Topic created.\n");
                 leave_critical_region_topic(i);
                 return true;
             }
         }
         return true;
     }else{
-        printf("Topic size is %d, max amount reached\n",topicsSize);
-        printf("Topic %s not created \n", name);
+        printf("Topic size is %d, max amount reached.\n",topicsSize);
+        printf("Topic %s not created.\n", name);
         return false;
     }
 }
@@ -238,7 +190,7 @@ bool delete_topic(const char * name){
             return true;
         }
     }
-    printf("Topic size is %d, max amount reached\n",topicsSize);
+    printf("Topic size is %d, max amount reached.\n",topicsSize);
     printf("Unable to delete topic %s.\n", name);
     return false;
 }
@@ -274,7 +226,9 @@ void create_new_user_topic(const int id, const char * name){
  */
 void publish_into_user_topic(const UserTopic * userTopic, const char * msg, const int msgLocation){
     printf("Publishing into a user topic\n");
-    strcpy(userTopic->messageContent[msgLocation],msg);
+    char *a = malloc(sizeof(msg));
+    strcpy(a,msg);
+    userTopic->messageContent[msgLocation] = a;
     //    userTopic->read[msgLocation] = false;
     printf("UserTopic created\n");
 }
--- servers/pm/project_2_syst_call.h
+++ servers/pm/project_2_syst_call.h
@@ -2,7 +2,56 @@
 #ifndef PROJECT_2_SYST_CALL_H
 #define PROJECT_2_SYST_CALL_H
 
+#include <signal.h>
+
+#include <sys/resource.h>
+#include <sys/utsname.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <assert.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/wait.h>
+
+#define MAX_MSG 5	/* Number of message for each topic */
+#define MAX_TOPIC 10	/* Number of allowed topics for each topic */
+#define MAX_USR 30	/* Number of allowed user */
+
+typedef int semaphore;	/* Define semaphore as a type */
+
 typedef enum {false = 0, true = 1} bool;
+
+typedef struct UserTopic{
+    int id; /* Topic id */
+    char * name;    /* Topic name */
+    char * messageContent[MAX_MSG]; /* Message content of a Topic */
+    bool read[MAX_MSG]; /* If the message was read */
+    void (*toString)(const struct UserTopic *); /* Pointer to the display function of a UserTopic*/
+}UserTopic;
+
+typedef struct Subscriber{
+    pid_t pid_subscriber;   /* Subscriber pid_t */
+    struct UserTopic topic[MAX_TOPIC];   /* Topics subscribed */
+    void (*toString)(const struct Subscriber *);    /* Pointer to the display function of a Subscriber */
+}Subscriber;
+
+typedef struct Publisher{
+    pid_t pid_publisher;   /* Publisher pid_t */
+    char * topicNames[MAX_TOPIC];   /* Topics declared as published */
+    void (*toString)(const struct Publisher *);    /* Pointer to the display function of a Publisher */
+}Publisher;
+
+typedef struct Topics{
+    char * topicNames[MAX_TOPIC];    /* List of all topics */
+    bool canBeRemoved[MAX_TOPIC];    /* If a topic can be removed */
+    void (*toString)(const struct Topics *);  /* Pointer to the display function of a Topics */
+}Topics;
 
 int do_topic_lookup(void);
 
@@ -18,8 +67,10 @@ int do_retrieve(void);
 
 bool create_topic(const char * name);
 
+void create_new_user_topic(const int id, const char * name);
+
 bool delete_topic(const char * name);
 
-void create_new_user_topic(const int id, const char * name);
+void publish_into_user_topic(const UserTopic * userTopic, const char * msg, const int msgLocation);
 
 #endif /** PROJECT_2_SYST_CALL_H */

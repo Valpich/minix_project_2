44611f5b6521c099d80baf272b5f3d424d4fcce4
Jules Baud <baud.jules@gmail.com>
2017-03-30 13:35:29 -0500

    lookup

--- servers/pm/project_2_syst_call.c
+++ servers/pm/project_2_syst_call.c
@@ -145,6 +145,15 @@ void leave_critical_region_topic(int topic_id){
 }
 
 int do_topic_lookup(void){
+    if(topics != NULL){
+        int i = 0; 
+        for(i= 0; i< MAX_TOPIC;i++){
+            printf("Topic #%d : %s\n",i,topics->topicNames[i]);
+        }
+    }
+    else {
+        printf("Topics is null\n");
+    }
     return 1;
 }
 

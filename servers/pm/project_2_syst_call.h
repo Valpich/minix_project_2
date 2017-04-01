46d30bb4bd2a7c39781df18dd2789689a1519e50
AFA16SCM17R <aregnier@hawk.iit.edu>
2017-03-31 21:11:37 -0500

    Commentaries
    Add commentaries in project_2_syst_call.h

--- servers/pm/project_2_syst_call.h
+++ servers/pm/project_2_syst_call.h
@@ -27,125 +27,131 @@
 #ifdef  MINIX
 
 #endif
-#define MAX_MSG 5   /* Number of message for each topic */
-#define MAX_TOPIC 10    /* Number of allowed topics for each topic */
-#define MAX_USR 30  /* Number of allowed user */
-#define MAX_TOPIC_NAME 20
-#define MAX_MSG_CONTENT 100
+#define MAX_MSG 5                                       /* Number of message for each topic */
+#define MAX_TOPIC 10                                    /* Number of allowed topics for each topic */
+#define MAX_USR 30                                      /* Number of allowed user */
+#define MAX_TOPIC_NAME 20                               /* Number of characters allowed for topic name */
+#define MAX_MSG_CONTENT 100                             /* Number of characters allowed for topic content */
 
-#define INVALID_POSITION -1
-#define INVALID_PID -1
-#define INVALID_ID -1
-#define TOPIC_NOT_FOUND -2
-#define NO_SLOT_AVAILABLE -3
-#define USR_NOT_REGISTRED_AS_PUBLISHER -4
-#define MSG_NOT_PUBLISHED_SLOTS_FULL -5
-#define NOT_ENOUGH_SPACE_IN_PUBLISHER_SLOTS -6
-#define INVALID_TOPIC_NAME -7
-#define ALREADY_REGISTRED -8
-#define FAIL_TO_REGISTER -9
+#define INVALID_POSITION -1                             /* Constant declaration for Invalid Position code return */
+#define INVALID_PID -1                                  /* Constant declaration for Invalid Pid code return */
+#define INVALID_ID -1                                   /* Constant declaration for Invalid Id code return */
+#define TOPIC_NOT_FOUND -2                              /* Constant declaration for Topic Not Found code return */
+#define NO_SLOT_AVAILABLE -3                            /* Constant declaration for No Slot Available code return */
+#define USR_NOT_REGISTRED_AS_PUBLISHER -4               /* Constant declaration for User Not Registred As Publisher code return */
+#define MSG_NOT_PUBLISHED_SLOTS_FULL -5                 /* Constant declaration for Message Not Published because Slots are Full code return */
+#define NOT_ENOUGH_SPACE_IN_PUBLISHER_SLOTS -6          /* Constant declaration for Not Enough Space In Publisher Slots code return */
+#define INVALID_TOPIC_NAME -7                           /* Constant declaration for Invalid Topin Name code return */
+#define ALREADY_REGISTRED -8                            /* Constant declaration for Already Registred code return */
+#define FAIL_TO_REGISTER -9                             /* Constant declaration for Fail To Register code return */
 
-#define NEW_REGISTRATION 100
-#define PUBLISHER_REGISTRED 101
-#define MSG_PUBLISHED 102
+#define NEW_REGISTRATION 100                            /* Constant declaration for New Registration code return */
+#define PUBLISHER_REGISTRED 101                         /* Constant declaration for Publisher Registred code return */
+#define MSG_PUBLISHED 102                               /* Constant declaration for Message Published code return */
 
-#define DO_TOPIC_LOOKUP_SUCCESS_RETURN 200
-#define DO_TOPIC_CREATE_SUCCESS_RETURN 201
-#define DO_TOPIC_CREATE_FAILURE_RETURN -201
-#define DO_TOPIC_PUBLISHER_FAILURE_RETURN -202
-#define DO_TOPIC_SUBSCRIBER_SUCCESS_RETURN 203
-#define DO_TOPIC_SUBSCRIBER_FAILURE_RETURN -203
-#define DO_RETRIEVE_SUCCESS_RETURN 204
-#define DO_RETRIEVE_FAILURE_RETURN -204
-#define DO_PRINT_SUBSCRIBERS_SUCCESS_RETURN 205
-#define DO_PRINT_PUBLISHERS_SUCCESS_RETURN 206
-#define DO_TOPIC_DELETE_SUCCESS_RETURN 207
-#define DO_TOPIC_DELETE_FAILURE_RETURN -207
+#define DO_TOPIC_LOOKUP_SUCCESS_RETURN 200              /* Constant declaration for Do Topic Lookup Success code return */
+#define DO_TOPIC_CREATE_SUCCESS_RETURN 201              /* Constant declaration for Do Topic Create Success code return */
+#define DO_TOPIC_CREATE_FAILURE_RETURN -201             /* Constant declaration for Do Topic Create Failure code return */
+#define DO_TOPIC_PUBLISHER_FAILURE_RETURN -202          /* Constant declaration for Do Topic Publisher Failure code return */
+#define DO_TOPIC_SUBSCRIBER_SUCCESS_RETURN 203          /* Constant declaration for Do Topic Subscriber Success code return */
+#define DO_TOPIC_SUBSCRIBER_FAILURE_RETURN -203         /* Constant declaration for Do Topic Subscriber Failure code return */
+#define DO_RETRIEVE_SUCCESS_RETURN 204                  /* Constant declaration for Do Topic Retrieve Success code return */
+#define DO_RETRIEVE_FAILURE_RETURN -204                 /* Constant declaration for Do Topic Retrieve Failure code return */
+#define DO_PRINT_SUBSCRIBERS_SUCCESS_RETURN 205         /* Constant declaration for Do Topic Subscribers Success code return */
+#define DO_PRINT_PUBLISHERS_SUCCESS_RETURN 206          /* Constant declaration for Do Topic Publishers Success code return */
+#define DO_TOPIC_DELETE_SUCCESS_RETURN 207              /* Constant declaration for Do Topic Delete Success code return */
+#define DO_TOPIC_DELETE_FAILURE_RETURN -207             /* Constant declaration for Do Topic Delete Failure code return */
 
-typedef int semaphore;  /* Define semaphore as a type */
+typedef int semaphore;                                  /* Define semaphore as a type */
 
-typedef enum {false = 0, true = 1} bool;
+typedef enum {false = 0, true = 1} bool;                /* Define boolean type bool */
 
-typedef struct UserTopic{
-    int id; /* Topic id */
-    char * name;    /* Topic name */
-    char * messageContent[MAX_MSG]; /* Message content of a Topic */
-    bool read[MAX_MSG]; /* If the message was read */
-    void (*toString)(const struct UserTopic *); /* Pointer to the display function of a UserTopic*/
+typedef struct UserTopic{                               /* Define UserTopic as a structure */
+    int id;                                             /* Topic id */
+    char * name;                                        /* Topic name */
+    char * messageContent[MAX_MSG];                     /* Message content of a Topic */
+    bool read[MAX_MSG];                                 /* Boolean read to know if the message was read */
+    void (*toString)(const struct UserTopic *);         /* Pointer to the display function of a UserTopic*/
 }UserTopic;
 
-typedef struct Subscriber{
-    pid_t pid_subscriber;   /* Subscriber pid_t */
-    struct UserTopic userTopic[MAX_TOPIC];   /* Topics subscribed */
-    void (*toString)(const struct Subscriber *);    /* Pointer to the display function of a Subscriber */
+typedef struct Subscriber{                              /* Define Subscriber as a structure */
+    pid_t pid_subscriber;                               /* Subscriber pid_t */
+    struct UserTopic userTopic[MAX_TOPIC];              /* Topics subscribed */
+    void (*toString)(const struct Subscriber *);        /* Pointer to the display function of a Subscriber */
 }Subscriber;
 
-typedef struct Publisher{
-    pid_t pid_publisher;   /* Publisher pid_t */
-    char * topicNames[MAX_TOPIC];   /* Topics declared as published */
-    void (*toString)(const struct Publisher *);    /* Pointer to the display function of a Publisher */
+typedef struct Publisher{                                /* Define Publisher as a structure */
+    pid_t pid_publisher;                                 /* Publisher pid_t */
+    char * topicNames[MAX_TOPIC];                        /* Topics declared as published */
+    void (*toString)(const struct Publisher *);          /* Pointer to the display function of a Publisher */
 }Publisher;
 
-typedef struct Topic{
-    int id; /* Topic id */
-    char * name;    /* List of all topics */
-    bool msgSlotAvailable[MAX_MSG];    /* If a message can be removed */
-    void (*toString)(const struct Topic *);  /* Pointer to the display function of a Topic */
+typedef struct Topic{                                    /* Define Topic as a structure */
+    int id;                                              /* Topic id */
+    char * name;                                         /* List of all topics */
+    bool msgSlotAvailable[MAX_MSG];                      /* If a message can be removed */
+    void (*toString)(const struct Topic *);              /* Pointer to the display function of a Topic */
 }Topic;
 
-typedef struct Topics{
-    Topic topicArray[MAX_TOPIC];    /* List of all topics */
-    bool canBeRemoved[MAX_TOPIC];    /* If a Topic can be removed */
-    void (*toString)(const struct Topics *);  /* Pointer to the display function of a Topics */
+typedef struct Topics{                                   /* Define Topics as a structure, that contains structure topic */
+    Topic topicArray[MAX_TOPIC];                         /* List of all topics */
+    bool canBeRemoved[MAX_TOPIC];                        /* If a Topic can be removed */
+    void (*toString)(const struct Topics *);             /* Pointer to the display function of a Topics */
 }Topics;
 
 /********* BEGIN OF TO STRING FUNCTIONS **********/
 
-void toStringUserTopic(const UserTopic * userTopic);
+void toStringUserTopic(const UserTopic * userTopic);     /* Display available UserTopic */
 
-void toStringSubscriber(const Subscriber * subscriber);
+void toStringSubscriber(const Subscriber * subscriber);  /* Display available Subscribers */
 
-void toStringPublisher(const Publisher * publisher);
+void toStringPublisher(const Publisher * publisher);     /* Display available Publishers*/
 
-void toStringTopic(const Topic * topic);
+void toStringTopic(const Topic * topic);                 /* Display available Topic */
 
-void toStringTopics(const Topics * topic);
+void toStringTopics(const Topics * topic);               /* Display available Topics */
 
-void toStringAllPublisher(const Publisher publisher[]);
+void toStringAllPublisher(const Publisher publisher[]); /* Display all Publishers */
 
-void toStringAllSubscriber(const Subscriber subscriber[]);
+void toStringAllSubscriber(const Subscriber subscriber[]);  /* Display all Subscribers */
 
 /********* END OF TO STRING FUNCTIONS **********/
 
 /********* BEGIN OF SEMAPHORE METHODS **********/
 
-void down(semaphore * s);
+void down(semaphore * s);                                 /* Give back Semaphore */
 
-void up(semaphore * s);
+void up(semaphore * s);                                   /* Take Semaphore */
 
-void enter_critical_region_topic(int topic_id);
+void enter_critical_region_topic(int topic_id);           /* Enter the critical region for specified Topic id */
 
-void leave_critical_region_topic(int topic_id);
+void leave_critical_region_topic(int topic_id);           /* Leave the critical region for specified Topic id */
 
-void checkNotDown(semaphore * s);
+void checkNotDown(semaphore * s);                         /* Check that the Semaphore haven't been giving back */
 
-void wait_read_critical_region_topic(int topic_id);
+void wait_read_critical_region_topic(int topic_id);       /* Wait that other process have finished to read the critical region of specified Topic id */
 
 /********* END OF SEMAPHORE METHODS **********/
 
 /********* BEGIN OF SYSTEM CALL METHODS **********/
 
-int do_topic_lookup(void);
+int do_topic_lookup(void);                                /* Syscall of do_topic_lookup function */
 
-int do_topic_create(void);
+int do_topic_create(void);                                /* Syscall of do_topic_create function */
 
-int do_topic_publisher(void);
+int do_topic_publisher(void);                             /* Syscall of do_topic_publisher function */
 
-int do_topic_subscriber(void);
+int do_topic_subscriber(void);                            /* Syscall of do_topic_subscriber function */
 
-int do_topic_publish(void);
+int do_topic_publish(void);                               /* Syscall of do_topic_publish function */
 
-int do_retrieve(void);
+int do_retrieve(void);                                    /* Syscall of do_retrieve function */
+
+int do_print_subscribers(void);                           /* Syscall of do_print_subscribers function */
+
+int do_print_publishers(void);                            /* Syscall of do_print_publishers function */
+
+int do_topic_delete(void);                                /* Syscall of do_topic_delete function */
 
 /********* END OF SYSTEM CALL METHODS **********/
 
@@ -153,53 +159,53 @@ int do_retrieve(void);
 
 /********* BEGIN OF UTILITY METHODS **********/
 
-int doInit();
+int doInit();                                                                           /* Initialization of Publishers, Subscribers and Topics */
 
-Topic * findTopicByName(const char * user_pid);
+Topic * findTopicByName(const char * user_pid);                                         /* Return a Topic id for a specified Topic name */
 
-Subscriber * findSubscriberByPid(const pid_t user_pid);
+Subscriber * findSubscriberByPid(const pid_t user_pid);                                 /* Return a Subscriber name for a specified Subscriber pid */
 
-Publisher * findPublisherById(pid_t user_pid);
+Publisher * findPublisherById(pid_t user_pid);                                          /* Return a Publisher name for a specified Publisher id */
 
-int findUserTopicPosition(const Subscriber * subscriber, const Topic * topic);
+int findUserTopicPosition(const Subscriber * subscriber, const Topic * topic);          /* Return the position of a Subscriber for a specified Topic */
 
-int findAndLockAvailableSlot(Topic * topic);
+int findAndLockAvailableSlot(Topic * topic);                                            /* Return if there is an available slot for a Topic, if possible, it's locking the position for this Topic */
 
-bool checkAllRetrieved(const Topic * topic, const int slot);
+bool checkAllRetrieved(const Topic * topic, const int slot);                            /* Return if all Messages have been Retrieved by the Subscribers for a specified Topic */
 
-bool userIsRegistredAsPublisher(const char * topicName, const Publisher * publisher);
+bool userIsRegistredAsPublisher(const char * topicName, const Publisher * publisher);   /* Return if an User is Registred As A Publisher for a specified Publisher and a Specified Topic */
 
-int is_ID_set(const char * name, pid_t user_pid);
+int is_ID_set(const char * name, pid_t user_pid);                                       /* Return if a Subscriber have is ID set for a Topic for a specified Subscriber name and pid */
 
-int subscribers_init(const char * name, pid_t user_pid);
+int subscribers_init(const char * name, pid_t user_pid);                                /* Return the Initialization of a Subscriber for a specified Topic */
 
 /********* END OF UTILITY METHODS **********/
 
 /********* BEGIN OF CORE METHODS **********/
 
-void lookup();
+void lookup();                                                                                       /* Display the list of all Topics Available*/
 
-bool create_topic(const char * name);
+bool create_topic(const char * name);                                                                /* Create a Topic for a Specified Name */
 
-int topic_publisher(const char * name, pid_t user_pid);
+int topic_publisher(const char * name, pid_t user_pid);                                              /* Set a specified User as a Publisher by is id for a selected Topic */
 
-bool subscribe_to_topic(const char * name, pid_t user_pid);
+bool subscribe_to_topic(const char * name, pid_t user_pid);                                          /* Set a specified User as a Subscriber by is id of a selected Topic*/
 
-void publish_into_user_topic(UserTopic * userTopic, const char * msg, const int msgLocation);
+void publish_into_user_topic(UserTopic * userTopic, const char * msg, const int msgLocation);        /* Publish a message into a specified UserTopic */
 
-int publish_into_all_user_topic(Topic * topic, const char * msg);
+int publish_into_all_user_topic(Topic * topic, const char * msg);                                    /* Publish a message into all UserTopic */
 
-int publish_msg_into_topic(const char * topicName, const char * msg, const Publisher * publisher);
+int publish_msg_into_topic(const char * topicName, const char * msg, const Publisher * publisher);   /* Publish a message into a specified Topic */
 
-char * retrieve_msg_of_topic(const pid_t user_pid, const char * topicName);
+char * retrieve_msg_of_topic(const pid_t user_pid, const char * topicName);                          /* Retrieve a message for a specified Subscriber for a a specified Topic */
 
-char * readMessage(UserTopic *userTopic);
+char * readMessage(UserTopic *userTopic);                                                            /* Read a Message in a specified UserTopic */
 
-bool delete_topic(const char * name);
+bool delete_topic(const char * name);                                                                /* Delete a specified Topic */
 
-void printAllPublishers();
+void printAllPublishers();                                                                           /* Display the list of All Registered Publishers */
 
-void printAllSubscribers();
+void printAllSubscribers();                                                                          /* Display the list of All Registered Subscribers */
 
 /********* END OF CORE METHODS **********/
 
@@ -207,7 +213,7 @@ void printAllSubscribers();
 
 /********* BEGIN OF DEBUG METHODS **********/
 
-void toStringData();
+void toStringData();                                                                                 /* Display all the Data available */
 
 /********* END OF DEBUG METHODS **********/
 
